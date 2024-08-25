ef142e3c3b32fccbb10559ed2b693ef307606e89
peterhuang569 <peterzdhuang@gmail.com>
2024-08-24 23:16:24 -0600

    added the discovery and config servers

---
+++ services/config-sever/src/test/java/com/peter/configsever/ConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.configsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
