2a51a9c5d6f7895e52f96af8bae48a0ee77c8684
peterhuang569 <peterzdhuang@gmail.com>
2024-08-27 23:48:24 -0600

    commented

--- services/discovery/src/main/java/com/peter/discovery/DiscoveryApplication.java
+++ services/discovery/src/main/java/com/peter/discovery/DiscoveryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
-
+	// Discovery app
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
